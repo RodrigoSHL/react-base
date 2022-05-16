3a68ef743734b2e546d7c83b7432aef5fefa182d
Rodrigo Catalán López <ext_recatalanl@falabella.cl>
2022-05-15 20:01:40 -0400

    fix: fixing import error in test navigation
    Signed-off-by: Rodrigo Catalán López <ext_recatalanl@falabella.cl>

--- src/routes/test/Navigation.test.tsx
+++ src/routes/test/Navigation.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import Navigation from './Navigation';
+import Navigation from '../Navigation';
 
 test('renders learn react link', () => {
   render(<Navigation />);
